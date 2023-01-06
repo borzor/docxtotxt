11e05bcd0ce436cdfe387ad7182092e59c1048b7
borzor <borzor@mail.ru>
2023-01-06 05:04:43 -0800

    Added changes, added flag for raw tables, document notes parser, warning fixes

---
+++ Libs/tinyxml2/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/borzor/CLionProjects/tinyxml2
+# Build directory: /home/borzor/CLionProjects/tinyxml2/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(xmltest "/home/borzor/CLionProjects/tinyxml2/cmake-build-debug/xmltest")
+set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "/home/borzor/CLionProjects/tinyxml2" _BACKTRACE_TRIPLES "/home/borzor/CLionProjects/tinyxml2/CMakeLists.txt;48;add_test;/home/borzor/CLionProjects/tinyxml2/CMakeLists.txt;0;")
